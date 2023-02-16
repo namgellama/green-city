3268a901ed59aab7ee52e764e10caba119f807a2
Namgel Tamang <namgeltamang14@gmail.com>
2023-02-16 16:47:35 +0545

    Change footer text.

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,8 +2,8 @@ function Footer() {
 	const footerYear = new Date().getFullYear();
 
 	return (
-		<footer className="footer p-6 bg-black text-white text-center">
-			<p>Copyright &copy; {footerYear}, GreenCity, All rights reserved </p>
+		<footer className="footer p-5 bg-black text-white text-center">
+			<p>Copyright &copy; {footerYear}, GreenCity</p>
 		</footer>
 	);
 }
